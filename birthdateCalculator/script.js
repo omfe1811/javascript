89fedf2a8af35480b0fed6c6e7fe78edc7c528a9
Olga <omfe1811@gmail.com>
2024-12-19 15:03:43 +0100

    Update script.js

--- birthdateCalculator/script.js
+++ birthdateCalculator/script.js
@@ -2,20 +2,25 @@
 let buttoncap = document.getElementById("buttoncap");
 let getButton = document.getElementById("startstopbutton");
 let howMuchTime = document.getElementById("timePassed");
+let calendarInput = document.getElementById("calendar");
 let today = new Date();
 function calculateTime() {
   let birthday = new Date(calendarInput.value);
   let timeSinceBirthMili = today - birthday;
-  let timeSinceBirthYears = timeSinceBirthMili / 1000 / 60 / 60 / 24 / 365;
-  let timeSinceBirthMonths = timeSinceBirthMili / 1000 / 60 / 60 / 24;
-  let timeSinceBirthDays = timeSinceBirthMili / 1000 / 60 / 60;
+  let timeSinceBirthYears = timeSinceBirthMili / 1000 / 60 / 24 / 12 / 365;
+  let timeSinceBirthMonths = timeSinceBirthMili / 1000 / 60 / 24 / 12;
+  let timeSinceBirthDays = timeSinceBirthMili / 1000 / 60 / 24;
   let timeSinceBirthHours = timeSinceBirthMili / 1000 / 60;
   let timeSinceBirthSeconds = timeSinceBirthMili / 1000;
-  console.log(
-    "Years since your birth: " + timeSinceBirthYears,
-    "\nMonths since your birth: " + timeSinceBirthMonths,
-    "\nDays since your birth: " + timeSinceBirthDays,
-    "\nHours since your birth: " + timeSinceBirthHours,
-    "\nSeconds since your birth: " + timeSinceBirthSeconds
-  );
+  howMuchTime.innerText =
+    "Years since your birth: " +
+    timeSinceBirthYears +
+    "\nMonths since your birth: " +
+    timeSinceBirthMonths +
+    "\nDays since your birth: " +
+    timeSinceBirthDays +
+    "\nHours since your birth: " +
+    timeSinceBirthHours +
+    "\nSeconds since your birth: " +
+    timeSinceBirthSeconds;
 }
