4a60483ecc682372a24e3ff531c74d5bdbd827f0
Olga <omfe1811@gmail.com>
2024-12-20 10:23:58 +0100

    bibday

--- birthdateCalculator/script.js
+++ birthdateCalculator/script.js
@@ -5,6 +5,7 @@ let getButton = document.getElementById("startstopbutton");
 let howMuchTime = document.getElementById("timePassed");
 let calendarInput = document.getElementById("calendar");
 let today = new Date();
+
 //calculating time passed
 function calculateTime() {
   //take date from html input
@@ -20,9 +21,9 @@ function calculateTime() {
   //how many years days/365
   let timeSinceBirthYears = timeSinceBirthDays / 365;
   /// how many months passed years/52
-  let timeSinceBirthMonths = timeSinceBirthYears / 52;
-
-  howMuchTime.innerText =
+  let timeSinceBirthMonths = timeSinceBirthYears / 12;
+  //display calculations as a message
+  let message = (howMuchTime.innerText =
     "Years since your birth: " +
     timeSinceBirthYears +
     "\nMonths since your birth: " +
@@ -32,5 +33,19 @@ function calculateTime() {
     "\nHours since your birth: " +
     timeSinceBirthHours +
     "\nSeconds since your birth: " +
-    timeSinceBirthSeconds;
+    timeSinceBirthSeconds);
+
+  // display birthday message and calculations
+  let birthdayMessage = "Happy Birthday!!!\n" + message;
+
+  //check if today is the birthday
+  if (
+    birthday.getMonth() == today.getMonth() &&
+    birthday.getDate() == today.getDate()
+  ) {
+    howMuchTime.innerText = birthdayMessage;
+  } else {
+    //if its not the birthday display just the calculations
+    howMuchTime.innerText = message;
+  }
 }
