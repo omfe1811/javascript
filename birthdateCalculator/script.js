f337421b3a459fa009b3c3d9baeb1d834398b8a3
Olga <omfe1811@gmail.com>
2024-12-20 09:27:24 +0100

    Update script.js

--- birthdateCalculator/script.js
+++ birthdateCalculator/script.js
@@ -1,16 +1,25 @@
 //oppgave how many days passed since 05.06.98
+//coneecting html to js
 let buttoncap = document.getElementById("buttoncap");
 let getButton = document.getElementById("startstopbutton");
 let howMuchTime = document.getElementById("timePassed");
 let calendarInput = document.getElementById("calendar");
 let today = new Date();
+//calculating time passed
 function calculateTime() {
+  //take date from html input
   let birthday = new Date(calendarInput.value);
+  //how much time has passed since the set day til today
   let timeSinceBirthMili = today - birthday;
-  let timeSinceBirthYears = timeSinceBirthMili / 1000 / 60 / 60 / 24 / 365;
-  let timeSinceBirthMonths = timeSinceBirthMili / 1000 / 60 / 60 / 24 / 12;
-  let timeSinceBirthDays = timeSinceBirthMili / 1000 / 60 / 60 / 24;
-  let timeSinceBirthHours = timeSinceBirthMili / 1000 / 60 / 60;
+  //how many years
+  let timeSinceBirthYears = timeSinceBirthHours / 24 / 365;
+  //how many months
+  let timeSinceBirthMonths = timeSinceBirthHours / 24 / 12;
+  //how many days
+  let timeSinceBirthDays = timeSinceBirthHours / 24;
+  //how many hours
+  let timeSinceBirthHours = timeSinceBirthSeconds / 60 / 60;
+  //how many seconds
   let timeSinceBirthSeconds = timeSinceBirthMili / 1000;
   howMuchTime.innerText =
     "Years since your birth: " +
