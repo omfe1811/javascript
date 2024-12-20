af60bd1a9dfd93b0d1582c4a5b722f264045026c
Olga <omfe1811@gmail.com>
2024-12-20 09:22:19 +0100

    Update script.js

--- birthdateCalculator/script.js
+++ birthdateCalculator/script.js
@@ -7,10 +7,10 @@ let today = new Date();
 function calculateTime() {
   let birthday = new Date(calendarInput.value);
   let timeSinceBirthMili = today - birthday;
-  let timeSinceBirthYears = timeSinceBirthMili / 1000 / 60 / 24 / 12 / 365;
-  let timeSinceBirthMonths = timeSinceBirthMili / 1000 / 60 / 24 / 12;
-  let timeSinceBirthDays = timeSinceBirthMili / 1000 / 60 / 24;
-  let timeSinceBirthHours = timeSinceBirthMili / 1000 / 60;
+  let timeSinceBirthYears = timeSinceBirthMili / 1000 / 60 / 60 / 24 / 365;
+  let timeSinceBirthMonths = timeSinceBirthMili / 1000 / 60 / 60 / 24 / 12;
+  let timeSinceBirthDays = timeSinceBirthMili / 1000 / 60 / 60 / 24;
+  let timeSinceBirthHours = timeSinceBirthMili / 1000 / 60 / 60;
   let timeSinceBirthSeconds = timeSinceBirthMili / 1000;
   howMuchTime.innerText =
     "Years since your birth: " +
